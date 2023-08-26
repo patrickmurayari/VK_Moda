0f750dffe6706bcf982652ccd6f5ad2d54f7e3a3
joak990 <joakhaidar@gmail.com>
2023-08-26 12:46:57 -0300

    acomodando

--- vk_moda/src/components/Categorias/Bolsos.jsx
+++ vk_moda/src/components/Categorias/Bolsos.jsx
@@ -66,11 +66,11 @@ const Bolsos = () => {
         <div className="relative min-h-screen ">
             
             <NavCategoria />
-            <div className=" flex justify-center mt-20 md:mt-20">
+            <div className=" mt-20 md:mt-20">
             <h1 className="text-black font-extralight  font-montserrat_alternates text-2xl mt-10 md:text-4xl">BOLSOS</h1>
             </div>
-            <div className="mt-20">
-                <div className="grid grid-cols-1  md:grid-cols-4 gap-4 md:gap-10">
+            <div className="lg:mt-14">
+                <div className="grid grid-cols-1  md:grid-cols-4 gap-10 md:gap-10">
                     {products &&
                         products.map((elem, index) => (
                             <div
@@ -78,7 +78,7 @@ const Bolsos = () => {
                                 className="flex flex-col  items-center justify-center w-full h-96 p-8 md:p-8  transition-transform transform-gpu hover:scale-105 hover:transition-transform duration-300"
                             >
                                 <img
-                                    className="hover:brightness-50 mt-1 object-cover h-auto  md:h-96 md:w-screen"
+                                    className="hover:brightness-50 mt-1 object-cover h-96 md:h-96 md:w-screen"
                                     src={elem.image}
                                     alt="producto"
                                 />
@@ -92,7 +92,7 @@ const Bolsos = () => {
                 </div>
                
             </div>
-            <div className="flex justify-center gap-2 md:mt-6">
+            <div className="flex justify-center gap-2  mt-10 md:mt-6">
                     <img className="w-10 h-10" src={insta} alt="" />
                     <img className ="w-10 h-10" src={facebook} alt="" />
                 </div>
